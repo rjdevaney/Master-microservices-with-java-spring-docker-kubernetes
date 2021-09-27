7f38c98a81509a7d824bd14209de12eb54618a16
Eazy Bytes <tutor@eazybytes.com>
2021-09-27 15:58:02 +0530

    Thanks for choosing to learn from EazyBytes

---
+++ section2/helloworldservice - Copy/src/main/java/com/eazybytes/helloworldservice/HelloworldserviceApplication.java
@@ -0,0 +1,13 @@
+package com.eazybytes.helloworldservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldserviceApplication.class, args);
+	}
+
+}
