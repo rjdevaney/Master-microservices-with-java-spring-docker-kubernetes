484460df42e9585bc6abe5e1458e0c9c8700b233
Eazy Bytes <tutor@eazybytes.com>
2021-10-01 15:55:08 +0530

    Thanks for choosing to learn from EazyBytes

---
+++ section8/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.eazybytes.loans.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eazybytes.loans.config.LoansServiceConfig;
+import com.eazybytes.loans.model.Customer;
+import com.eazybytes.loans.model.Loans;
+import com.eazybytes.loans.model.Properties;
+import com.eazybytes.loans.repository.LoansRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+/**
+ * @author Eazy Bytes
+ *
+ */
+
+@RestController
+public class LoansController {
+
+	@Autowired
+	private LoansRepository loansRepository;
+	
+	@Autowired
+	LoansServiceConfig loansConfig;
+
+	@PostMapping("/myLoans")
+	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+		List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+		if (loans != null) {
+			return loans;
+		} else {
+			return null;
+		}
+
+	}
+	
+	@GetMapping("/loans/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(loansConfig.getMsg(), loansConfig.getBuildVersion(),
+				loansConfig.getMailDetails(), loansConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
+	}
+
+}
