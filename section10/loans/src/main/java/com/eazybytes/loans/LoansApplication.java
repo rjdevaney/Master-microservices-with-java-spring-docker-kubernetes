3bb60efb049db665d3a0e65e48b4f12142922592
Eazy Bytes <tutor@eazybytes.com>
2021-10-07 23:25:54 +0530

    Implementing Spring Cloud Gateway

---
+++ section10/loans/src/main/java/com/eazybytes/loans/LoansApplication.java
@@ -0,0 +1,22 @@
+package com.eazybytes.loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@RefreshScope
+@ComponentScans({ @ComponentScan("com.eazybytes.loans.controller") })
+@EnableJpaRepositories("com.eazybytes.loans.repository")
+@EntityScan("com.eazybytes.loans.model")
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
