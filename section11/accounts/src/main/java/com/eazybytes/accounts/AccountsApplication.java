add2d675336f1fcf7c8bda4d92ed3deec923525a
Eazy Bytes <tutor@eazybytes.com>
2021-10-08 23:18:03 +0530

    Distributed Tracing & Log Aggregation

--- section11/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ section11/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -5,13 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import io.micrometer.core.aop.TimedAspect;
-import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -23,11 +19,6 @@ public class AccountsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsApplication.class, args);
-	}
-	
-	@Bean
-	public TimedAspect timedAspect(MeterRegistry registry) {
-	    return new TimedAspect(registry);
 	}
 
 }
