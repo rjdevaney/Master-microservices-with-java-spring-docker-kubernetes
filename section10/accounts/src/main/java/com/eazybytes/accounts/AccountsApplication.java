7bc4c0b28be9fb89c06b760767052d1e611c1ace
Eazy Bytes <tutor@eazybytes.com>
2021-10-08 23:16:07 +0530

    Implementing Spring Cloud Gateway

--- section10/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ section10/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -5,13 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import io.micrometer.core.aop.TimedAspect;
-import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -25,9 +21,5 @@ public class AccountsApplication {
 		SpringApplication.run(AccountsApplication.class, args);
 	}
 	
-	@Bean
-	public TimedAspect timedAspect(MeterRegistry registry) {
-	    return new TimedAspect(registry);
-	}
 
 }
