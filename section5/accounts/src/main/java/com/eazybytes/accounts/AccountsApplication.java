cbc5fe6856b952041b67b9e0858ea19807926804
Eazy Bytes <tutor@eazybytes.com>
2021-10-08 23:07:33 +0530

    Make your microservices Docker compatible

--- section5/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ section5/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -3,13 +3,9 @@ package com.eazybytes.accounts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import io.micrometer.core.aop.TimedAspect;
-import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller")})
@@ -19,11 +15,6 @@ public class AccountsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsApplication.class, args);
-	}
-	
-	@Bean
-	public TimedAspect timedAspect(MeterRegistry registry) {
-	    return new TimedAspect(registry);
 	}
 
 }
